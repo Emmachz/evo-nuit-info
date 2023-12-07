1ad472f6fddfea8ce38386f83012e7745be6c023
Myriam1501 <myriamo93500@gmail.com>
2023-12-07 20:43:59 +0100

    new branch

---
+++ src/app/solution/solution.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-solution',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './solution.component.html',
+  styleUrl: './solution.component.scss'
+})
+export class SolutionComponent {
+
+}
